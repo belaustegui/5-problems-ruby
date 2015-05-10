84d5b1cd9d16fa0e4ab4e1a96cc9b56e1e64c846
belaustegui <cristian.alvb@gmail.com>
2015-05-10 21:30:25 +0200

    Fix Solution for Third Problem
    The problem consists in calculating the Fibonacci sequence up to the
    100th element, not in calculating only the 100th element itself.

--- src/problem-3/problem-3.rb
+++ src/problem-3/problem-3.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
 
-# This variable contains precomputed values for the Fibonacci sequence in order
-# to avoid recalculating them multiple times.
-$precomputed = [0, 1];
-
-# Calculate the number in the given position of the Fibonacci sequence
-def solve(position)
-  return $precomputed[position] if !$precomputed[position].nil?
-  result = solve(position -1) + solve(position - 2)
-  $precomputed[position] = result
-  return result
+# Calculate the Fibonacci sequence up to the number in the given position
+# Instead of using the traditional recursive solution, wich has a complexity of
+# O(n^2), the use of a list to store the already calculated values reduces the
+# complexity to O(n).
+def solve(number)
+  @sequence = [0, 1]
+  (2 ... number).each do |position|
+    @sequence[position] = @sequence[position - 1] + @sequence[position - 2]
+  end
+  @sequence
 end
 
-puts solve 100
+solve(100).each_with_index do |item, index|
+  puts "#{index + 1} => #{item}"
+end
